efaf6d8a272776ce588b6d163cdf1039f1ad432d
Stefan Dellmuth <stefan.dellmuth@msg-systems.com>
2018-01-19 11:01:47 +0100

    Projekt mit Spring Initializer generiert

---
+++ src/main/java/de/msg/gbg/hackathon18/navigalypse/NavigalypseApplication.java
@@ -0,0 +1,12 @@
+package de.msg.gbg.hackathon18.navigalypse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavigalypseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavigalypseApplication.class, args);
+	}
+}
